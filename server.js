cee53b784b1fc96bb78cec3921de9545bf99911f
Bruno <lrpmblizzard@gmail.com>
2021-11-11 20:00:53 -0300

    cualit-prueba-back

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+const db = require('./app/models');
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop ans re-sync db");
+});
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the test." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
